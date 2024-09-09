8c1cd160ce2feb8e13224527d45065519a61a078
Sumit2608 <103249029+Sumit2608@users.noreply.github.com>
2024-09-08 23:04:05 -0700

    Add files via upload

---
+++ Embeddable1/src/main/java/beans/Address.java
@@ -0,0 +1,27 @@
+package beans;
+
+import javax.persistence.Embeddable;
+
+@Embeddable
+public class Address {
+    private  int house_no;
+    private String city;
+
+    public int getHouse_no() {
+        return house_no;
+    }
+
+    public void setHouse_no(int house_no) {
+        this.house_no = house_no;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+
+}
---
+++ Embeddable1/src/main/java/beans/Student.java
@@ -0,0 +1,50 @@
+package beans;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Student_table")
+public class Student {
+    @Id
+    private int id;
+    private String name;
+    private Address address;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+
+
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", address=" + address +
+                '}';
+    }
+}
